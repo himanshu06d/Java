9e50fae200bd015ba12fb506ee2628c41f30b13b
Himanshu Dubey <hd156681@gmail.com>
2021-03-17 23:19:05 +0530

    solving loop problem

---
+++ loopProblem1.java
@@ -0,0 +1,18 @@
+
+public class loopProblem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=4;i>0;i--)
+{
+	for(int j=0;j<i;j++)
+	{
+	System.out.print("*");
+	
+}
+	System.out.print("\n");
+	}
+
+}
+	}
+
