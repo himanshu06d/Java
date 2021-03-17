0d351592754156a60de81227f8adcc6e842b068d
Himanshu Dubey <hd156681@gmail.com>
2021-03-17 23:14:01 +0530

    while loop

---
+++ whileLoop.java
@@ -0,0 +1,13 @@
+
+public class whileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 100;
+while(a<201) {
+	System.out.println(a);
+	a++;
+}
+	}
+
+}
