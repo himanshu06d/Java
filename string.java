b6f1d94992c285c21f0c37f6281c4cdaef0d1a3c
Himanshu Dubey <hd156681@gmail.com>
2021-03-17 23:09:07 +0530

    string

---
+++ string.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.out.println("please enter your name");
+Scanner sc = new Scanner(System.in);
+String name = sc.next();
+sc.close();
+System.out.println("your name is "+name);
+
+
+	}
+
+}
