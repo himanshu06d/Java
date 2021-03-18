c85923f1abeb9e2332e14c0fdebe065825d758ad
Himanshu Dubey <hd156681@gmail.com>
2021-03-18 14:05:16 +0530

    sum of numbers in array

---
+++ Array_problem1.java
@@ -0,0 +1,17 @@
+
+public class Array_problem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float [] numbers= {10.5f,17.89f,4.5f};
+		float sum=0;
+		for(int i=0;i<numbers.length;i++)
+		{
+		sum+=numbers[i];
+			System.out.println(numbers[i]);
+		}
+		System.out.println(sum);
+
+	}
+
+}
