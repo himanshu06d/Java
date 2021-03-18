da08d940aeb8e9299a6066f9bae3636696483903
Himanshu Dubey <hd156681@gmail.com>
2021-03-18 14:07:10 +0530

    Finding max number in a given array

---
+++ Array_problem3.java
@@ -0,0 +1,20 @@
+
+public class Array_problem3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int [] arr = {1,45,67,89,99};
+int max=0;
+for(int e:arr)
+{
+	if(e>max)
+	{
+		max=e;
+		}
+	
+}
+System.out.println(max);
+
+	}
+
+}
