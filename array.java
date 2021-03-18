d89f519d3cdfb1f10fb4dae0f068bf7d89c9dee5
Himanshu Dubey <hd156681@gmail.com>
2021-03-18 14:07:54 +0530

    understanding basic array implementation

---
+++ array.java
@@ -0,0 +1,22 @@
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//there are many ways to initialize an array 
+		//declaration
+		int [] marks;
+		marks=new int [5]; //initialize
+	//ystem.out.print(marks);
+		
+//declaration +initialization
+		int [] marks1 = new int [5];
+	//ystem.out.println(marks1);
+		// declaration+initialization+memory allocation
+	int [] marks2 = {80,90,70};
+	//stem.out.println(marks2);
+	System.out.println(marks2.length);
+	}
+
+}
